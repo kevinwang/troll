95bba05fa3b0c4bd8050a271617fe380bd5dadbf
Eric Lam <captainpicardlam@gmail.com>
2013-01-24 15:27:03 -0500

    Add untested filecheck for the index.

--- index.c
+++ index.c
@@ -27,3 +27,29 @@ void add_to_index(char *hash, char *path) {
     close(indexfd);
     free(fullpath);
 }
+
+int index_file_check(char *hash, char *path) {
+    char *indexpath = get_repo_troll_dir();
+    int repo_path_len = strlen(indexpath) - 7;
+    indexpath = (char *) realloc(indexpath, strlen(indexpath) + 5);
+    strcat(indexpath, "index");
+
+    char *fullpath = realpath(path, NULL);
+    char *relpath = fullpath + repo_path_len;
+    
+    FILE *findex = fopen(indexpath, "r");
+    free(indexpath);
+    lseek(indexfd, 0, SEEK_SET);
+    
+    char str[256];
+    while(*fgets(str, 256, findex) != EOF) {
+      if(strstr(str,hash)) {
+	return 0;
+      }
+      if(strstr(str,path)) {
+	return 1;
+      }
+    }
+    fclose(findex);
+    return -1;
+}
