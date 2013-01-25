ff68fc5c280511f9cb79963adad3e539a983b1d4
Kevin Wang <born2run5000@yahoo.com>
2013-01-25 03:38:46 -0500

    Fix warnings and errors in status.c.

--- status.c
+++ status.c
@@ -5,13 +5,58 @@
 #include <sys/types.h>
 #include <dirent.h>
 #include <openssl/sha.h>
+#include <unistd.h>
+#include <sys/stat.h>
 #include "commands.h"
 #include "path.h"
 #include "index.h"
+#include "object.h"
+
+char *file_hash(char *filename) {
+    int fd;
+    fd = open(filename, O_RDONLY);
+    struct stat buf;
+    fstat(fd, &buf);
+    char *data = (char *) malloc((size_t) buf.st_size + 15);
+    sprintf(data, "blob %lu", (unsigned long) buf.st_size);
+    int left_part_len = strlen(data) + 1;
+    read(fd, data + left_part_len, buf.st_size);
+    int full_len = left_part_len + buf.st_size;
+    char *hash = hash_object(data, full_len);
+    close(fd);
+    return hash;
+}
+
+void search_dirs(int unstaged, int untracked, char *curr) {
+    DIR *dir = opendir(curr);
+    struct dirent *dirstuff;
+    while((dirstuff = readdir(dir))) {
+      printf("%s\n",dirstuff->d_name);
+      if(dirstuff->d_type == DT_DIR && *(dirstuff->d_name) != '.') {
+	char *currnew = (char *)malloc(strlen(curr) + strlen(dirstuff->d_name)+1);
+	strcat(currnew, dirstuff->d_name);
+	strcat(currnew, "/");
+	printf("currnew: %s\n",currnew);
+	search_dirs(unstaged,untracked,currnew);
+      }
+      else {
+	int ind = index_file_check(file_hash(dirstuff->d_name),dirstuff->d_name);
+	printf("name: %s\n",dirstuff->d_name);
+	if(ind < 0) {
+	  write(unstaged,dirstuff->d_name, strlen(dirstuff->d_name));
+	  write(unstaged,"\n",1);
+	}
+	else if(ind > 0) {
+	  write(untracked,dirstuff->d_name,strlen(dirstuff->d_name));
+	  write(untracked,"\n",1);
+	}
+      }
+    }   
+}
 
 int cmd_status(int argc, const char *argv[]) {
     die_if_not_troll_repo();
-    char *currdir = getcwd();
+    char *currdir = getcwd(NULL, 0);
     char *trollpath = get_repo_troll_dir();
     char *usta = (char *)malloc(strlen(trollpath) + 12);
     strcat(usta, trollpath);
@@ -38,46 +83,4 @@ int cmd_status(int argc, const char *argv[]) {
     //    unlink("unstaged.tmp");
     //unlink("untracked.tmp");    
     return 0;
-}
-
-char *file_hash(char *filename) {
-    int fd;
-    fd = open(filename, O_RDONLY);
-    struct stat buf;
-    fstat(fd, &buf);
-    char *data = (char *) malloc((size_t) buf.st_size + 15);
-    sprintf(data, "blob %lu", (unsigned long) buf.st_size);
-    int left_part_len = strlen(data) + 1;
-    read(fd, data + left_part_len, buf.st_size);
-    int full_len = left_part_len + buf.st_size;
-    char *hash = hash_object(data, full_len);
-    close(fd);
-    return hash;
-}
-
-void search_dirs(int unstaged, int untracked, char *curr) {
-    DIR *dir = opendir(curr);
-    struct dirent *dirstuff;
-    while(dirstuff = readdir(dir)) {
-      printf("%s\n",dirstuff->d_name);
-      if(dirstuff->d_type == DT_DIR && *(dirstuff->d_name) != '.') {
-	char *currnew = (char *)malloc(strlen(curr) + strlen(dirstuff->d_name)+1);
-	strcat(currnew, dirstuff->d_name);
-	strcat(currnew, "/");
-	printf("currnew: %s\n",currnew);
-	search_dirs(unstaged,untracked,currnew);
-      }
-      else {
-	int ind = index_file_check(file_hash(dirstuff->d_name),dirstuff->d_name);
-	printf("name: %s\n",dirstuff->d_name);
-	if(ind < 0) {
-	  write(unstaged,dirstuff->d_name, strlen(dirstuff->d_name));
-	  write(unstaged,"\n",1);
-	}
-	else if(ind > 0) {
-	  write(untracked,dirstuff->d_name,strlen(dirstuff->d_name));
-	  write(untracked,"\n",1);
-	}
-      }
-    }   
 }
