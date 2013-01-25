1de8921892335a31ce1ff719a1e29cb488e97a98
Eric Lam <captainpicardlam@gmail.com>
2013-01-25 11:31:14 -0500

    Make checking out work, overwriting current files.

--- checkout.c
+++ checkout.c
@@ -14,6 +14,7 @@
 
 int cmd_checkout(int argc, const char *argv[]) {
     new_index((char *)(argv[1]));
+    copy_out();
     return 0;
 }
 
@@ -21,6 +22,7 @@ void new_index(char *hash) {
     char *indexpath = get_repo_troll_dir();
     indexpath = (char *) realloc(indexpath, strlen(indexpath) + 5);
     strcat(indexpath, "index");
+    printf("New index at %s\n",indexpath);
 
     int index = open(indexpath, O_WRONLY | O_TRUNC, 0644);
     free(indexpath);
@@ -30,4 +32,45 @@ void new_index(char *hash) {
 
     print_object(hash);
     dup2(sout,STDOUT_FILENO);
+}
+
+void copy_out() {
+    char *indexpath = get_repo_troll_dir();
+    indexpath = (char *) realloc(indexpath, strlen(indexpath) + 5);
+    strcat(indexpath, "index");
+
+    FILE *findex = fopen(indexpath, "r");
+    free(indexpath);
+    fseek(findex, 0, SEEK_SET);
+    
+    char hash[41];
+    
+    int newfile;
+    char *trollpath = get_repo_troll_dir();
+    char *repo_path = (char *) malloc(strlen(trollpath) - 7);
+    strncat(repo_path,trollpath,strlen(trollpath) - 7);
+
+    free(trollpath);
+    char *newpath;
+    char name[256];
+    int sout;
+    char *lastchar;
+    sout = dup(STDOUT_FILENO);
+    while(fgets(hash, 41, findex)) {
+      fseek(findex,1,SEEK_CUR);
+      fgets(name, 256, findex);
+
+      newpath = (char *) malloc(strlen(repo_path) + strlen(name));
+      strcat(newpath, repo_path);
+      strcat(newpath, name);
+      lastchar = strstr(newpath,"\n");
+      *lastchar = '\0';
+
+      newfile = open(newpath, O_WRONLY | O_TRUNC, 0644);
+      printf("%s\n",newpath);
+      dup2(newfile,STDOUT_FILENO);
+      print_object(hash);
+      dup2(sout,STDOUT_FILENO);
+      close(newfile);
+    }
 }
--- checkout.h
+++ checkout.h
@@ -3,5 +3,6 @@
 
 int cmd_checkout(int argc, const char *argv[]);
 void new_index(char *hash);
+void copy_out();
 
 #endif
